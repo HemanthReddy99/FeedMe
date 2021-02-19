6c3ec291a4a2dcaa2adc890c94522a77ee585427
HemanthReddy69 <hemanthsai392@gmail.com>
2021-02-19 14:59:03 +0530

    Added login and Registration

---
+++ app/src/main/java/com/anupam/androidcameraxtflite/Login.java
@@ -0,0 +1,49 @@
+package com.anupam.androidcameraxtflite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    EditText e1,e2;
+    Button b;
+    TextView t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        e1 = findViewById(R.id.student_email_login_text_view);
+        e2 = findViewById(R.id.student_password_login_text_view);
+        b = findViewById(R.id.student_login_button);
+        t = findViewById(R.id.register);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = e1.getText().toString();
+                String password = e2.getText().toString();
+
+
+                    Toast.makeText(getApplicationContext(),"Successfully Login",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+            }
+        });
+
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Please enter details",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/anupam/androidcameraxtflite/Register.java
@@ -0,0 +1,83 @@
+package com.anupam.androidcameraxtflite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Register extends AppCompatActivity {
+
+    EditText Smail,Sfname,Smname,Slname,Sdob,Sphno,Sdept,SID,Scgpa,Sadd1,Sadd2,Sadd3,Spass,Srepass;
+    Button Sregister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Smail = findViewById(R.id.email_student_registration_text_view);
+        Sfname = findViewById(R.id.first_name_student_registration_text_view);
+        Smname = findViewById(R.id.middle_name_student_registration_text_view);
+        Slname = findViewById(R.id.last_name_student_registration_text_view);
+        Sdob = findViewById(R.id.dob_student_registration_text_view);
+        Sphno = findViewById(R.id.phone_student_registration_text_view);
+        Sdept = findViewById(R.id.department_student_registration_text_view);
+        SID = findViewById(R.id.rollno_student_registration_text_view);
+        Scgpa = findViewById(R.id.cgpa_student_registration_text_view);
+        Sadd1 = findViewById(R.id.addr1_student_registration_text_view);
+        Sadd2 = findViewById(R.id.addr2_student_registration_text_view);
+        Sadd3 = findViewById(R.id.addr3_student_registration_text_view);
+        Spass = findViewById(R.id.password_student_registration_text_view);
+        Srepass = findViewById(R.id.password_reenter_student_registration_text_view);
+
+        Sregister = findViewById(R.id.register_student_button);
+
+        Sregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = Smail.getText().toString();
+                String fname = Sfname.getText().toString();
+                String mname = Smname.getText().toString();
+                String lname = Slname.getText().toString();
+                String dob = Sdob.getText().toString();
+                String phno = Sphno.getText().toString();
+                String dept = Sdept.getText().toString();
+                String rollno = SID.getText().toString();
+                String cgpa  = Scgpa.getText().toString();
+                String add1 = Sadd1.getText().toString();
+                String add2 = Sadd2.getText().toString();
+                String add3 = Sadd3.getText().toString();
+                String pass = Spass.getText().toString();
+                String repass = Srepass.getText().toString();
+
+                if(email.equals("") || fname.equals("") || mname.equals("") || lname.equals("") || dob.equals("") || phno.equals("") || dept.equals("") || rollno.equals("") || cgpa.equals("") || add1.equals("") || add2.equals("") || add3.equals("") || pass.equals("") || repass.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(), "One or more fields empty", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    if(pass.equals(repass))
+                    {
+
+
+                                Toast.makeText(getApplicationContext(),"Registered Successfully", Toast.LENGTH_SHORT).show();
+                                Intent suc_reg =new Intent(getBaseContext(), Login.class);
+                                startActivity(suc_reg);
+
+                        }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Passwords do not Match", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+
+            }
+        });
+    }
+}
